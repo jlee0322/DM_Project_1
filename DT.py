e6452a1cb13034c40577a6bc7d5a8d22c96a401f
李仲哲 <69305533+jlee0322@users.noreply.github.com>
2022-10-05 10:33:02 +0800

    add files
    3 files:
    DT.py
    DT_2.py
    plot_cost_complexity_pruning.py

---
+++ DT.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 19:57:48 2022
+
+@author: acer
+"""
+
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+
+# 讀入鳶尾花資料
+iris = load_iris()
+iris_X = iris.data
+iris_y = iris.target
+
+# 切分訓練與測試資料
+train_X, test_X, train_y, test_y = train_test_split(iris_X, iris_y, test_size = 0.3)
+
+# 建立分類器
+clf = tree.DecisionTreeClassifier()
+iris_clf = clf.fit(train_X, train_y)
+
+# 預測
+test_y_predicted = iris_clf.predict(test_X)
+print(test_y_predicted)
+
+# 標準答案
+print(test_y)
